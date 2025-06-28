3b3ee645ab3d48f0c35853258544118d5c73f3ad
im-ksc <chan_kin_seng@hotmail.com>
2025-06-29 01:14:59 +0800

    chore: Enabled CrossOrigin

--- src/main/java/nus/iss/Main.java
+++ src/main/java/nus/iss/Main.java
@@ -4,8 +4,13 @@ import io.dropwizard.configuration.ResourceConfigurationSourceProvider;
 import io.dropwizard.core.Application;
 import io.dropwizard.core.setup.Bootstrap;
 import io.dropwizard.core.setup.Environment;
+import jakarta.servlet.DispatcherType;
+import jakarta.servlet.FilterRegistration;
 import nus.iss.resource.CoinCalculatorResource;
 import nus.iss.service.CoinCalculatorService;
+import org.eclipse.jetty.servlets.CrossOriginFilter;
+
+import java.util.EnumSet;
 
 public class Main extends Application<MainConfiguration> {
 
@@ -21,6 +26,16 @@ public class Main extends Application<MainConfiguration> {
 
     @Override
     public void run(MainConfiguration mainConfiguration, Environment environment) {
+
+        final FilterRegistration.Dynamic cors =
+                environment.servlets().addFilter("CORS", CrossOriginFilter.class);
+
+        cors.setInitParameter(CrossOriginFilter.ALLOWED_ORIGINS_PARAM, "*");
+        cors.setInitParameter(CrossOriginFilter.ALLOWED_HEADERS_PARAM, "X-Requested-With,Content-Type,Accept,Origin,Authorization");
+        cors.setInitParameter(CrossOriginFilter.ALLOWED_METHODS_PARAM, "OPTIONS,GET,PUT,POST,DELETE,HEAD");
+
+        cors.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
+
         CoinCalculatorService coinCalculatorService = new CoinCalculatorService();
         environment.jersey().register(new CoinCalculatorResource(coinCalculatorService));
     }
