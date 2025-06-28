b4e18096e85ee016049a95c08c638de80b4539e4
im-ksc <chan_kin_seng@hotmail.com>
2025-06-29 01:24:22 +0800

    chore: Enabled CrossOrigin

--- src/main/java/nus/iss/Main.java
+++ src/main/java/nus/iss/Main.java
@@ -6,11 +6,10 @@ import io.dropwizard.core.setup.Bootstrap;
 import io.dropwizard.core.setup.Environment;
 import jakarta.servlet.DispatcherType;
 import jakarta.servlet.FilterRegistration;
+import java.util.EnumSet;
 import nus.iss.resource.CoinCalculatorResource;
 import nus.iss.service.CoinCalculatorService;
 import org.eclipse.jetty.servlets.CrossOriginFilter;
-
-import java.util.EnumSet;
 
 public class Main extends Application<MainConfiguration> {
 
@@ -31,8 +30,11 @@ public class Main extends Application<MainConfiguration> {
                 environment.servlets().addFilter("CORS", CrossOriginFilter.class);
 
         cors.setInitParameter(CrossOriginFilter.ALLOWED_ORIGINS_PARAM, "*");
-        cors.setInitParameter(CrossOriginFilter.ALLOWED_HEADERS_PARAM, "X-Requested-With,Content-Type,Accept,Origin,Authorization");
-        cors.setInitParameter(CrossOriginFilter.ALLOWED_METHODS_PARAM, "OPTIONS,GET,PUT,POST,DELETE,HEAD");
+        cors.setInitParameter(
+                CrossOriginFilter.ALLOWED_HEADERS_PARAM,
+                "X-Requested-With,Content-Type,Accept,Origin,Authorization");
+        cors.setInitParameter(
+                CrossOriginFilter.ALLOWED_METHODS_PARAM, "OPTIONS,GET,PUT,POST,DELETE,HEAD");
 
         cors.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
 
